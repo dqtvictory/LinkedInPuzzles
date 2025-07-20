ca04f335333ece3e199c2d6ac369602e3b3f3aa9
Quang Trung Dam <trungdam@yahoo.com>
2025-07-20 09:33:12 +0000

    core parts ok

---
+++ LinkedInSolver.Client/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Microsoft.AspNetCore.Components;
+global using Microsoft.AspNetCore.Components.Web;
+global using MudBlazor;
---
+++ LinkedInSolver.Client/Models/Puzzle.cs
@@ -0,0 +1,10 @@
+namespace LinkedInSolver.Client.Models
+{
+    public class Puzzle
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Icon { get; set; } = string.Empty;
+        public string Route { get; set; } = string.Empty;
+    }
+}
