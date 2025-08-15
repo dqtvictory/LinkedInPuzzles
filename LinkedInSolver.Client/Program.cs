4e016bb72c974863a8491b2a6d86f1246fe4aae6
Damien Dam <qdam@student.42.fr>
2025-08-16 00:17:26 +0200

    try fixing 100% circle

--- LinkedInSolver.Client/Program.cs
+++ LinkedInSolver.Client/Program.cs
@@ -1,7 +1,10 @@
+using LinkedInSolver.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.RootComponents.Add<App>("#app");
 
 builder.Services.AddMudServices();
 
